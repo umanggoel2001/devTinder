91e75bb82370b30533dd797cd7e24116b4b503a8
Umang Goel <umang.1923it1013@kiet.edu>
2025-01-22 01:09:18 +0530

    post get call

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const port =3000;
 app.use("/test",(req,res)=>{
     res.send("Hi Umang");
 })
+app.get("/user", (req,res)=>{
+    res.send("data coming from server");
+})
+
+app.post("/user", (req,res)=>{
+    res.send("Data saved to DB successfully");
+})
 app.listen(port,()=>{
     console.log("Server id running on 3000 Hello umang");
 })
